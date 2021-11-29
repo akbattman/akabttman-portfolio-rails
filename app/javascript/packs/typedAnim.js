14b70aeb877f809aeb1ed3770f9a05c27b666163
Aaron Battman <aaron.k.battman@outlook.com>
2021-11-29 16:30:13 +0000

    landing style re-design. typed-js implmtd. pallette scaling to refine

--- app/javascript/packs/typedAnim.js
+++ app/javascript/packs/typedAnim.js
@@ -3,9 +3,11 @@ import Typed from 'typed.js';
 const typeAnim = () => {
 
   var options = {
-    strings: ["//=> Fullstack Web App Dev", "<%# Software Engineer", "//=> Available to work", "<%# Contacts 'Something"],
+    strings: [  
+      "&ensp;=>&ensp; Fullstacker <br> &emsp;//&ensp; Web App Developer <br> &ensp;&emsp;&amp;&amp;&ensp; Software Engineer. <br> &emsp;&emsp;#&ensp; Available to interview <br> &emsp;&emsp;&ensp;||&ensp; Contact Me &ensp; :D &nbsp;"
+    ],
     typeSpeed: 80,
-    smartBackspace: true,
+    // smartBackspace: true,
     // loop: true,
     // loopCount: Infinity,
     showCursor: true,
