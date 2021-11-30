1b897cfaa692ff74447c7e57e9f490e061a1a7e2
Aaron Battman <aaron.k.battman@outlook.com>
2021-11-30 13:41:55 +0000

    pg cntrlr meths for all static pages & rooted. rendered shared banner for non built

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,8 @@
 class PagesController < ApplicationController
   def home; end
+  def projects; end
+  def about; end
+  def further_learning; end
+  def contact; end
+  def shoutouts; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "pages#home"
+  root to: "pages#home", as: :home
+  
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  get 'further_learning', to: 'pages#further_learning'
+  get 'projects', to: 'pages#projects'
+  get 'shoutouts', to: 'pages#shoutouts'
 end
